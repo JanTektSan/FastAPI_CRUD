5616a95b36659467ba66d58fb69871858a16f905
jantektsan <yuniverse9999@hotmail.com>
2023-12-01 10:01:47 +0900

    using path paramters and request body in one function

--- main.py
+++ main.py
@@ -26,6 +26,14 @@ async def read_user_me():
 async def read_user(user_id: str):
     return {"user_id": user_id}
 
-@app.post("/items")
+@app.post("/items/")
 async def create_item(item: Item):
-    return item
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+@app.put("/items/{item_id}")
+async def create_item(item_id: int, item: Item):
+    return {"item_id": item_id, **item.dict()}
